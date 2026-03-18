30243616bd1ce7f64ac120937e9522f61d450deb
mrsdolfin-lang <mrsdolfin@gmail.com>
2026-03-18 10:25:49 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-var CACHE='fizux-v1';
-var FILES=['./','./index.html','./fizux-app.js','./manifest.json'];
-self.addEventListener('install',function(e){self.skipWaiting();e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(FILES).catch(function(){});}))});
-self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(keys){return Promise.all(keys.filter(function(k){return k!==CACHE}).map(function(k){return caches.delete(k)}))}).then(function(){return self.clients.claim()}))});
-self.addEventListener('fetch',function(e){var url=e.request.url;if(url.indexOf('anthropic')>-1||url.indexOf('googleapis')>-1||url.indexOf('openai')>-1||url.indexOf('deepseek')>-1||url.indexOf('openrouter')>-1||url.indexOf('huggingface')>-1||url.indexOf('pollinations')>-1)return;e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request)}))});
